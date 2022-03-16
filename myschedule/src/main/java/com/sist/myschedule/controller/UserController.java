93ea12829c06b10a3b874712886d1576dc265a2e
KEUMIN <mskeum93@naver.com>
2022-03-16 12:42:25 +0900

    JWT 구현...

--- myschedule/src/main/java/com/sist/myschedule/controller/UserController.java
+++ myschedule/src/main/java/com/sist/myschedule/controller/UserController.java
@@ -3,6 +3,7 @@ package com.sist.myschedule.controller;
 import com.sist.myschedule.dto.ResponseDTO;
 import com.sist.myschedule.dto.UserDTO;
 import com.sist.myschedule.model.UserEntity;
+import com.sist.myschedule.security.TokenProvider;
 import com.sist.myschedule.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,9 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private TokenProvider tokenProvider;
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
@@ -44,9 +48,13 @@ public class UserController {
                 userDTO.getEmail(), userDTO.getPassword()
         );
         if (user != null) {
+            // 토큰 생성
+            final String token = tokenProvider.create(user);
+
             final UserDTO responseUserDTO = UserDTO.builder()
                     .email(user.getEmail())
                     .id(user.getId())
+                    .token(token)
                     .build();
             return ResponseEntity.ok().body(responseUserDTO);
         } else {
